2fe0bbeb045ee3aec8203e42af57a149dfad7874
IktaS <imamrafiia@gmail.com>
2020-02-21 17:02:06 +0700

    final ye

--- .v2/soal2/soal2_dekripsiv2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set1=`echo {z..a} {z..a} | tr -d " "`;
-set2=`echo {Z..A} {Z..A} | tr -d " "`;
-tempstring=$(echo $1 | cut -d "." -f 1 | tr ${set1:0:26}${set2:0:26} ${set1:$2:26}${set2:$2:26})
-mv $1 $tempstring.txt
--- .v2/soal2/soal2_enkripsiv2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set1=`echo {a..z} {a..z} | tr -d " "`;
-set2=`echo {A..Z} {A..Z} | tr -d " "`;
-tempstring=$(echo $1 | cut -d "." -f 1 | tr ${set1:0:26}${set2:0:26} ${set1:`date +%H`:26}${set2:`date +%H`:26})
-mv $1 $tempstring.txt
